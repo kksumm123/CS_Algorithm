d5dfab4db23658a9d5165b13a7223b048ec18a89
kksumm123 <kksumm123@gmail.com>
2022-01-09 22:39:37 +0900

    x만큼간격이있는n개의숫자 완료

--- x만큼간격이있는n개의숫자/Program.cs
+++ x만큼간격이있는n개의숫자/Program.cs
@@ -1,7 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace x만큼간격이있는n개의숫자
 {
+    public class Solution
+    {
+        public long[] solution(int x, int n)
+        {
+            long[] answer = new long[n];
+            for (int i = 0; i < n; i++)
+                answer[i] = x + (i * (long)x);
+
+            return answer;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
