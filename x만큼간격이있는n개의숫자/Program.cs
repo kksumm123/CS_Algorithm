564d0411923e05940f7f471f42d3d243b7035ddc
kksumm123 <kksumm123@gmail.com>
2022-01-09 22:15:21 +0900

    1. 직사각형별찍기 완료 2. x 만큼 간격이 있는 n개의 숫자 추가

---
+++ x만큼간격이있는n개의숫자/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace x만큼간격이있는n개의숫자
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
