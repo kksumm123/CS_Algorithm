1b6cbee9aa83a4ae532b5e3216099be5e2bf37bb
kksumm123 <kksumm123@gmail.com>
2022-01-14 23:28:51 +0900

    평균 구하기

---
+++ 평균구하기/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace 평균구하기
+{
+    public class Solution
+    {
+        public double solution(int[] arr)
+        {
+            double answer = 0;
+            for (int i = 0; i < arr.Length; i++)
+                answer += arr[i];
+
+            return answer / arr.Length;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
