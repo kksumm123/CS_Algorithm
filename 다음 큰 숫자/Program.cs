f665f0e9a5df23b98b8ec04a306bd6b802a95dca
kksumm123 <kksumm123@gmail.com>
2022-01-15 00:15:03 +0900

    레베루 2, 다음 큰 숫자

---
+++ 다음 큰 숫자/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Solution
+{
+    public int solution(int n)
+    {
+        int inputNumBitCount = GetBitCount(n);
+        int newBitCount = 0;
+        while (true)
+        {
+            newBitCount = GetBitCount(++n);
+            if (inputNumBitCount == newBitCount)
+                return n;
+        }
+    }
+
+    int GetBitCount(int n)
+    {
+        if (n == 0)
+            return 0;
+
+        return n % 2 + GetBitCount(n / 2);
+    }
+}
+
+namespace 다음_큰_숫자
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
