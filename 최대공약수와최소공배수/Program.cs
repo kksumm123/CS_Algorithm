5a1ea12d908f22656488e575b0b250e1faa4f558
kksumm123 <kksumm123@gmail.com>
2022-01-16 21:47:49 +0900

    최대공약수와 최소공배수

---
+++ 최대공약수와최소공배수/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace 최대공약수와최소공배수
+{
+    public class Solution
+    {
+        public int[] solution(int n, int m)
+        {
+            var gcd = GCD(n, m);
+            return new int[] { gcd, n * m / gcd }; ;
+        }
+
+        int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                var result = a % b;
+                a = b;
+                b = result;
+            }
+            return a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(3 % 12);
+        }
+    }
+}
