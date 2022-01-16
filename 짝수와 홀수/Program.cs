f1ad2a3f0ebc1c3b78f0656cfc4a0b04fd58319e
kksumm123 <kksumm123@gmail.com>
2022-01-16 21:59:12 +0900

    짝수와 홀수

---
+++ 짝수와 홀수/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 짝수와_홀수
+{
+    public class Solution
+    {
+        public string solution(int num) => num % 2 == 0 ? "Even" : "Odd";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
