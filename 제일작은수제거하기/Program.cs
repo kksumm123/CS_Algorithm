72439a8632f0ec82a5aa588ca3e155666ae33e76
kksumm123 <kksumm123@gmail.com>
2022-01-16 22:23:55 +0900

    제일 작은 수 제거하기

---
+++ 제일작은수제거하기/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace 제일작은수제거하기
+{
+    public class Solution
+    {
+        public int[] solution(int[] arr)
+        {
+            if (arr.Length == 1)
+            {
+                arr[0] = -1;
+                return arr;
+            }
+            var lii = arr.ToList();
+            lii.Remove(lii.Min());
+
+            return lii.ToArray();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
