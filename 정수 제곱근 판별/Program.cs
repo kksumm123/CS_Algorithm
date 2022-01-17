9fc2f935c5de64603d7266d56e48230a19c7b0f0
kksumm123 <kksumm123@gmail.com>
2022-01-18 00:42:55 +0900

    정수 제곱근 판별

---
+++ 정수 제곱근 판별/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 정수_제곱근_판별
+{
+    public class Solution
+    {
+        public long solution(long n)
+        {
+            long answer = 0;
+
+            int sqrtInt = Convert.ToInt32(Math.Sqrt(n));
+            if (Math.Pow(sqrtInt, 2) == n)
+            {
+                answer = (long)Math.Pow(sqrtInt + 1, 2);
+                return answer;
+            }
+
+            return -1;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
