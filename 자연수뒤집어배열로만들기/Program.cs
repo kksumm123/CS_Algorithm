cadf3f12312120f0570094d048b0631b3e93d6ac
kksumm123 <kksumm123@gmail.com>
2022-01-18 01:23:00 +0900

    자연수 뒤집어 배열로 만들기

---
+++ 자연수뒤집어배열로만들기/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 자연수뒤집어배열로만들기
+{
+    public class Solution
+    {
+        public int[] solution(long n) => SplitNumToArray(n);
+        int[] SplitNumToArray(long n)
+        {
+            List<int> nums = new List<int>();
+            while (n != 0)
+            {
+                int result = (int)(n % 10);
+
+                nums.Add(result);
+                n /= 10;
+            }
+
+            return nums.ToArray();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
