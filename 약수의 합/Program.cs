e0f4b68da85f6da54eb30d884a3a39134f54f0aa
kksumm123 <kksumm123@gmail.com>
2022-01-21 02:30:29 +0900

    약수의 합

---
+++ 약수의 합/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 약수의_합
+{
+    public class Solution
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                var r = n % i;
+                if (r == 0)
+                    answer += i;
+            }
+
+            return answer;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
