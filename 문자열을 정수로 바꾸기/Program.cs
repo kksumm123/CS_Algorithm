7ceb0fcea7f033f0101deb460123b671f4b496b3
kksumm123 <kksumm123@gmail.com>
2022-01-23 02:06:19 +0900

    문자열을 정수로 바꾸기

---
+++ 문자열을 정수로 바꾸기/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 문자열을_정수로_바꾸기
+{
+    public class Solution
+    {
+        public int solution(string s) => int.Parse(s);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+        }
+    }
+}
