4f83ce8f1b04e2cf7e108f5706ac4d6553ea7eec
kksumm123 <kksumm123@gmail.com>
2022-01-27 01:11:51 +0900

    수박수박수

---
+++ 수박수박수/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace 수박수박수
+{
+    public class Solution
+    {
+        public string solution(int n)
+        {
+            char[] result = new char[n];
+            for (int i = 0; i < n; i++)
+                result[i] = i % 2 == 0 ? '수' : '박';
+            return new string(result);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
