115ca5abf8f23d86603e69d825a7f1dfec2e8bf7
kksumm123 <kksumm123@gmail.com>
2022-02-06 21:05:38 +0900

    문자열 내림차순으로 배치하기

---
+++ 문자열 내림차순으로 배치하기/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace 문자열_내림차순으로_배치하기
+{
+public class Solution
+{
+    public string solution(string s)
+    {
+        var list = s.ToList();
+        list.Sort();
+        list.Reverse();
+        return new string(list.ToArray());
+    }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
