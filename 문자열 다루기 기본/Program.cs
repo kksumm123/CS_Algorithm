8fa3d0143cd538b3edb92feec0eb38dc2cd637bd
kksumm123 <kksumm123@gmail.com>
2022-02-06 20:44:36 +0900

    문자열 다루기 기본

---
+++ 문자열 다루기 기본/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace 문자열_다루기_기본
+{
+public class Solution
+{
+    public bool solution(string s)
+    {
+        if ((s.Length == 4 || s.Length == 6) == false)
+            return false;
+
+        int num = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            num = (int)(s[i] - '0');
+            if (num < 0 || num > 9)
+                return false;
+        }
+
+        return true;
+    }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+        }
+    }
+}
