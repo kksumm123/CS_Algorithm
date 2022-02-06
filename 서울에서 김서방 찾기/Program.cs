fde43a0917e975ca47dea13f27993f730090a19e
kksumm123 <kksumm123@gmail.com>
2022-02-06 20:04:10 +0900

    서울에서 김서방 찾기

---
+++ 서울에서 김서방 찾기/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace 서울에서_김서방_찾기
+{
+    public class Solution
+    {
+        public string solution(string[] seoul) => $"김서방은 {seoul.ToList().IndexOf("Kim")}에 있다";
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
