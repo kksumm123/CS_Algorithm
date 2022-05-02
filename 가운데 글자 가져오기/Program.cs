bdabeb7c2687b7b465a0b035e4f2db2259f47c40
kksumm123 <kksumm123@gmail.com>
2022-05-03 01:03:26 +0900

    가운데 글자 가져오기

---
+++ 가운데 글자 가져오기/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace 가운데_글자_가져오기
+{
+    class Program
+    {
+        public class Solution
+        {
+            public string solution(string s)
+            {
+                string answer = "";
+
+                var length = s.Length;
+                if ((length & 1) == 0)
+                {
+                    var targetIndex = length / 2;
+                    answer = $"{s[targetIndex - 1]}{s[targetIndex]}";
+                }
+                else
+                {
+                    var targetIndex = (length / 2);
+                    answer = $"{s[targetIndex]}";
+                }
+
+                return answer;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var test = new Solution();
+
+            Console.WriteLine(test.solution("abcde"));
+        }
+    }
+}
