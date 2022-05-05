e5d5e51648c069e1be1ec8aea3db32989538c9c9
kksumm123 <kksumm123@gmail.com>
2022-05-05 23:23:24 +0900

    부족한 금액 계산하기

---
+++ 부족한 금액 계산하기/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace 부족한_금액_계산하기
+{
+    class Program
+    {
+        class Solution
+        {
+            public long solution(int price, int money, int count)
+            {
+                long accumulatePrice = 0;
+                int currentPrice = 0;
+                for (int i = 1; i <= count; i++)
+                {
+                    currentPrice = price * i;
+                    accumulatePrice += currentPrice;
+                }
+
+                if (money > accumulatePrice)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return accumulatePrice - money;
+                }
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
