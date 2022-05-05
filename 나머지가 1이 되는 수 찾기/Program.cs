9e50bda1c0c32e59225b56595aff345bac36f09d
kksumm123 <kksumm123@gmail.com>
2022-05-05 23:30:43 +0900

    나머지가 1이 되는 수 찾기

---
+++ 나머지가 1이 되는 수 찾기/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace 나머지가_1이_되는_수_찾기
+{
+    class Program
+    {
+
+        public class Solution
+        {
+            public int solution(int n)
+            {
+                int answer = 0;
+                for (answer = 2; answer < n; answer++)
+                {
+                    if (n % answer == 1)
+                    {
+                        break;
+                    }
+                }
+
+                return answer;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
